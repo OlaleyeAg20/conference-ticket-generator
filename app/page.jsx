1618978c515183b4eb9c7c79e85819a7f5890b3b
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-02-11 23:28:35 +0100

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ const Ticketselection = ({ticketType, setTicketType, handleQuantityChange, ticke
             <h2>Techember Fest ”25</h2>
             <p>Join us for an unforgettable experience at Techember Fest ”25! Secure your spot now.</p>
             <div>
-              <span>📍 ASO || March 15, 2025 | 7:00 PM</span>
+              <span>📍 ASO Rock || March 15, 2025 | 7:00 PM</span>
             </div>
           </div>
 
