ff4c04a69dbd26a6949bd4c8bd9d1e9129b9d779
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-02-12 10:51:37 +0100

    new commit

--- app/components/Ticket.jsx
+++ app/components/Ticket.jsx
@@ -3,11 +3,51 @@ import React from 'react'
 const Ticket = () => {
   return (
     <div className='ticketContainer'>
-        <div></div>
+        {/* <div></div>
 
         <hr />
 
-        <div></div>
+
+        <div></div> */}
+
+        {/* Ticket divisor line */}
+        <div className='ticketDivisor'>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+            <span className="divisorNode"></span>
+        </div>
+
+
+        {/* Circle edges */}
+        <span className='edgeCircle edgeCircle1'></span>
+        <span className='edgeCircle edgeCircle2'></span>
+        <span className='edgeCircle edgeCircle3'></span>
+        <span className='edgeCircle edgeCircle4'></span>
+        <span className='edgeCircle edgeCircle5'></span>
+        <span className='edgeCircle edgeCircle6'></span>
     </div>
   )
 }
