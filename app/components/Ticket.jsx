fef781a74f75d4248ed139d23af6b85a78e2a7ea
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-02-12 12:19:08 +0100

    new commit

--- app/components/Ticket.jsx
+++ app/components/Ticket.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Ticket = () => {
   return (
@@ -9,6 +10,69 @@ const Ticket = () => {
 
 
         <div></div> */}
+
+        <div className="mainContainer">
+          <div className="innerMainContainer">
+            <div className='ticketHeaderDiv'>
+              <h2 className='ticketHeader'>Techember Fest ”25</h2>
+              <p>📍 04 Rumens road, Ikoyi, Lagos</p>
+              <p>📅 March 15, 2025 | 7:00 PM</p>
+            </div>
+
+            <Image className='profileImg' src={'https://ayscript.vercel.app/assets/avater-N-aruFBS.png'} width={140} height={200} alt='profile image' />
+
+            <div className="tableContainer">
+                <table className='customTable'>
+                  <tbody>
+                    <tr>
+                      <td>Row 1,<br /> Col 1</td>
+                      <td className='borderRow'>Row 1,<br /> Col 2</td>
+                    </tr>
+                    <tr>
+                      <td>Row 2,<br /> Col 1</td>
+                      <td className='borderRow'>Row 2,<br /> Col 2</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <p className='specialRequest'>Special request?</p>
+                <p className='specialRequestText'>
+                  Nil ? Or the users sad story they write in there gets this whole space, Max of three rows
+                </p>
+            </div>
+          </div>
+          <div className='barcodeDiv'>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode1'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+            <div className='barcode'></div>
+          </div>
+        </div>
 
         {/* Ticket divisor line */}
         <div className='ticketDivisor'>
@@ -33,11 +97,11 @@ const Ticket = () => {
             <span className="divisorNode"></span>
             <span className="divisorNode"></span>
             <span className="divisorNode"></span>
+            {/* <span className="divisorNode"></span>
             <span className="divisorNode"></span>
             <span className="divisorNode"></span>
             <span className="divisorNode"></span>
-            <span className="divisorNode"></span>
-            <span className="divisorNode"></span>
+            <span className="divisorNode"></span> */}
         </div>
 
 
